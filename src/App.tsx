e80f03249505314e8384d4d7cfb516244bd3c3c0
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-10-06 21:27:13 -0300

    Add: Initialized file reader process

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {useDispatch, useSelector} from "react-redux";
 import {createUser, selectUser} from "./store/user/actions";
 import {Layout, Table} from "antd";
 import { FooterComponent, HeaderComponent, SiderTable, TextAreaComponent } from './styles/styles';
-import { FolderOpenFilled, PlaySquareFilled, SaveFilled } from "@ant-design/icons"
+import { FolderOpenOutlined, PlaySquareOutlined, SaveOutlined } from "@ant-design/icons";
 
 const {Sider, Content} = Layout;
 
@@ -39,12 +39,40 @@ function App() {
         dispatch(createUser());
     }, []);
 
+    async function handleFileInput(target: EventTarget) {
+        debugger;
+        const $target = target as HTMLInputElement;
+        if (!$target.files) return;
+    
+        const textFile = await readFile($target.files[0]);
+                
+        // return the text file
+        console.log(textFile);
+    }
+    
+    async function readFile(file: File): Promise<string> {
+        return new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
+    
+            fileReader.onloadend = _ => resolve(String(fileReader.result));
+            fileReader.onerror = _ => reject();
+    
+            fileReader.readAsText(file);
+        });
+    }
+
     return (
         <Layout style={{height: '100%'}}>
             <HeaderComponent>
-                <FolderOpenFilled />
-                <SaveFilled />
-                <PlaySquareFilled />
+                <div 
+                    id="importFile"
+                    style={{display: "flex", cursor: "pointer"}}
+                    onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {handleFileInput(e.target)}}
+                >
+                    <FolderOpenOutlined />
+                </div>
+                <SaveOutlined style={{cursor: "pointer"}}/>
+                <PlaySquareOutlined style={{cursor: "pointer"}}/>
             </HeaderComponent>
             <Layout>
                 <Layout>
